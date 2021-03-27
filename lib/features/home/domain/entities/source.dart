dfd1d23cdbbdb6cc92f75b7639073ca39519d463
Yanuar Arifin <halo@yanuar.id>
2021-03-27 14:37:18 +0700

    add home repository, add mock home repository, add get source use case, add get source use case test, change dartz version, add source

---
+++ lib/features/home/domain/entities/source.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class Source extends Equatable {
+  final String id;
+  final String name;
+
+  Source(this.id, this.name);
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
