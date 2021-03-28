5424742d1aae3d974dd8e4f8079a949bdb68ebc3
Yanuar Arifin <halo@yanuar.id>
2021-03-28 17:25:05 +0700

    add discovery remote data source impl add article data source abstract extends discovery and home remote data source with article data source

--- lib/features/discovery/data/data_sources/discovery_remote_data_source.dart
+++ lib/features/discovery/data/data_sources/discovery_remote_data_source.dart
@@ -1,7 +1,38 @@
-import 'package:dartz/dartz.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:news_reader_app/core/constants/config.dart';
+import 'package:news_reader_app/core/data_sources/article_data_source.dart';
+import 'package:news_reader_app/core/errors/exceptions.dart';
 import 'package:news_reader_app/features/home/data/models/article_model.dart';
 
 abstract class DiscoveryRemoteDataSource {
-  Future<List<ArticleModel>> getNewsByKeyword(String keyword, int page,
-      int pageSize);
+  Future<List<ArticleModel>> getNewsByKeyword(
+      String keyword, int page, int pageSize);
+}
+
+class DiscoveryRemoteDataSourceImpl extends ArticleDataSource
+    implements DiscoveryRemoteDataSource {
+  final http.Client client;
+
+  DiscoveryRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<List<ArticleModel>> getNewsByKeyword(
+      String keyword, int page, int pageSize) async {
+    String params =
+        'apiKey=${Config.API_KEY}&q=$keyword&qInTitle=$keyword&page=$page&pageSize=$pageSize';
+
+    final response = await client.get(
+      Uri.parse(
+        '${Config.API_URL}/${Config.API_VERSION}/everything?$params',
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      return getTopHeadlinesModels(json.decode(response.body)['articles']);
+    } else {
+      throw HttpException(code: response.statusCode, message: response.body);
+    }
+  }
 }
