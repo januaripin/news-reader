366d162fb1ccc7152ffb599ce86df676fd50ee9c
Yanuar Arifin <halo@yanuar.id>
2021-03-28 19:22:27 +0700

    change home ui

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -31,115 +31,111 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          color: Colors.white,
-          child: MultiBlocProvider(
-            providers: [
-              BlocProvider(create: (_) => _sourceBloc),
-              BlocProvider(create: (_) => _topHeadlinesBloc),
-            ],
-            child: RefreshIndicator(
-              onRefresh: () async {
-                setState(() {
-                  _page = 1;
-                  _articles.clear();
-                });
-                _topHeadlinesBloc.add(
-                  GetTopHeadlinesEvent(
-                    sourceId: _selectedSourceId,
-                    page: _page,
-                    pageSize: _pageSize,
-                  ),
-                );
-              },
-              child: CustomScrollView(
-                slivers: [
-                  SliverList(
-                    delegate: SliverChildListDelegate([
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Align(
-                          child: Text(
-                            'US ${S().app_name}',
-                            textAlign: TextAlign.center,
-                            style:
-                                Theme.of(context).textTheme.headline5?.copyWith(
-                                      fontWeight: FontWeight.w700,
-                                    ),
-                          ),
-                        ),
-                      ),
-                    ]),
-                  ),
-                  SliverToBoxAdapter(
-                    child: BlocBuilder<SourceBloc, SourceState>(
-                      builder: (context, state) {
-                        if (state is SourceInitial) {
-                          _sourceBloc.add(GetSourceListEvent());
-                        } else if (state is GetSourceListError) {
-                          WidgetsBinding.instance?.addPostFrameCallback((_) {
-                            Utility.instance
-                                .showInfoDialog(context, state.message);
-                          });
-                        } else if (state is GetSourceListSuccess) {
-                          _page = 1;
-                          _articles.clear();
-                          WidgetsBinding.instance?.addPostFrameCallback((_) {
-                            _topHeadlinesBloc.add(GetTopHeadlinesEvent(
-                                sourceId: _selectedSourceId,
-                                page: _page,
-                                pageSize: _pageSize));
-                          });
-                          final sources = List.empty(growable: true);
-                          sources.add(Source("all", "All"));
-                          sources.addAll(state.sources);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          S().app_name,
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.headline5?.copyWith(
+                fontWeight: FontWeight.w700,
+              ),
+        ),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider(create: (_) => _sourceBloc),
+            BlocProvider(create: (_) => _topHeadlinesBloc),
+          ],
+          child: RefreshIndicator(
+            onRefresh: () async {
+              setState(() {
+                _page = 1;
+                _articles.clear();
+              });
+              _topHeadlinesBloc.add(
+                GetTopHeadlinesEvent(
+                  sourceId: _selectedSourceId,
+                  page: _page,
+                  pageSize: _pageSize,
+                ),
+              );
+            },
+            child: Column(
+              children: [
+                BlocBuilder<SourceBloc, SourceState>(
+                  builder: (context, state) {
+                    if (state is SourceInitial) {
+                      _sourceBloc.add(GetSourceListEvent());
+                    } else if (state is GetSourceListError) {
+                      WidgetsBinding.instance?.addPostFrameCallback((_) {
+                        Utility.instance.showInfoDialog(context, state.message);
+                      });
+                    } else if (state is GetSourceListSuccess) {
+                      _page = 1;
+                      _articles.clear();
+                      WidgetsBinding.instance?.addPostFrameCallback((_) {
+                        _topHeadlinesBloc.add(GetTopHeadlinesEvent(
+                            sourceId: _selectedSourceId,
+                            page: _page,
+                            pageSize: _pageSize));
+                      });
+                      final sources = List.empty(growable: true);
+                      sources.add(Source("all", "All"));
+                      sources.addAll(state.sources);
 
-                          return Container(
-                            height: 50,
-                            child: ListView.builder(
-                              padding: EdgeInsets.symmetric(horizontal: 8),
-                              scrollDirection: Axis.horizontal,
-                              itemCount: sources.length,
-                              itemBuilder: (context, index) {
-                                final source = sources[index];
-                                return SourceItem(
-                                  label: source.name,
-                                  onSelected: (b) {
-                                    _refreshPage(source.id);
-                                  },
-                                  selected: _selectedSourceId == source.id,
-                                );
+                      return Container(
+                        height: 50,
+                        child: ListView.builder(
+                          padding: EdgeInsets.symmetric(horizontal: 8),
+                          scrollDirection: Axis.horizontal,
+                          itemCount: sources.length,
+                          itemBuilder: (context, index) {
+                            final source = sources[index];
+                            return SourceItem(
+                              label: source.name,
+                              onSelected: (b) {
+                                _refreshPage(source.id);
                               },
-                            ),
-                          );
-                        }
+                              selected: _selectedSourceId == source.id,
+                            );
+                          },
+                        ),
+                      );
+                    }
 
-                        return Shimmer.fromColors(
-                          baseColor: Colors.grey[300]!,
-                          highlightColor: Colors.grey[100]!,
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              for (int i = 0; i < 4; i++)
-                                Chip(
-                                  label: Text('shimmer'),
-                                )
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  BlocBuilder<TopHeadlinesBloc, TopHeadlinesState>(
+                    return Shimmer.fromColors(
+                      baseColor: Colors.grey[300]!,
+                      highlightColor: Colors.grey[100]!,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          for (int i = 0; i < 4; i++)
+                            Chip(
+                              label: Text('shimmer'),
+                            )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                Expanded(
+                  child: BlocBuilder<TopHeadlinesBloc, TopHeadlinesState>(
                     builder: (context, state) {
                       if (state is GetTopHeadlinesError) {
                         WidgetsBinding.instance?.addPostFrameCallback((_) {
                           Utility.instance
                               .showInfoDialog(context, state.message);
                         });
+                      } else if (state is GetTopHeadlinesLoading) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
                       } else if (state is GetTopHeadlinesSuccess) {
                         _articles.addAll(state.topHeadlines);
                         _isLastPage = state.topHeadlines.length < _pageSize;
@@ -149,45 +145,36 @@ class _HomePageState extends State<HomePage> {
                         }
                       }
 
-                      return _articles.length == 0
-                          ? SliverToBoxAdapter(
-                              child: Center(
-                                child: CircularProgressIndicator(),
-                              ),
-                            )
-                          : SliverList(
-                              delegate: SliverChildBuilderDelegate(
-                                (context, index) {
-                                  if (index == _articles.length - 5 &&
-                                      !_isLastPage) {
-                                    _topHeadlinesBloc.add(GetTopHeadlinesEvent(
-                                        sourceId: _selectedSourceId,
-                                        page: _page,
-                                        pageSize: _pageSize));
-                                  }
+                      return ListView.builder(
+                        itemCount: _articles.length,
+                        itemBuilder: (context, index) {
+                          if (index == _articles.length - 5 && !_isLastPage) {
+                            _topHeadlinesBloc.add(GetTopHeadlinesEvent(
+                                sourceId: _selectedSourceId,
+                                page: _page,
+                                pageSize: _pageSize));
+                          }
 
-                                  return NewsItem(
-                                    article: _articles[index],
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (_) => ArticleDetailPage(
-                                              url: _articles[index].url),
-                                          settings: RouteSettings(
-                                              name: '/article-detail'),
-                                        ),
-                                      );
-                                    },
-                                  );
-                                },
-                                childCount: _articles.length,
-                              ),
-                            );
+                          return NewsItem(
+                            article: _articles[index],
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => ArticleDetailPage(
+                                      url: _articles[index].url),
+                                  settings:
+                                      RouteSettings(name: '/article-detail'),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      );
                     },
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
